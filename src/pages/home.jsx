8cc9c62c506d37ea367a932df1bae0cce75d98fb
EilonV <73593619+EilonV@users.noreply.github.com>
2023-05-09 16:21:08 +0300

    init

---
+++ src/pages/home.jsx
@@ -0,0 +1,23 @@
+import Spline from '@splinetool/react-spline';
+
+export const Home = () => {
+
+
+
+    return (
+        <section className='canvas-wrapper'>
+            <Spline scene="https://prod.spline.design/LJZ1gNK-OfLHoCpa/scene.splinecode" />
+            <div className="scene">
+                <div className="cube">
+                    <div className="cube__face cube__face--front"></div>
+                    <div className="cube__face cube__face--back"></div>
+                    <div className="cube__face cube__face--right"></div>
+                    <div className="cube__face cube__face--left"></div>
+                    <div className="cube__face cube__face--top"></div>
+                    <div className="cube__face cube__face--bottom"></div>
+                </div>
+            </div>
+
+        </section>
+    );
+}
