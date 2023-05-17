7434824eeaa85d69438b43ab651836e1437daf8e
EilonV <73593619+EilonV@users.noreply.github.com>
2023-05-17 14:38:36 +0300

    added location to determine active link in nav

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -1,18 +1,27 @@
 
+import { useEffect } from "react";
 import { Link } from "react-router-dom";
 import $ from 'jquery'
+import { useLocation } from 'react-router-dom'
 
 export const Nav = () => {
+    
+    const location = useLocation()
+
+    useEffect(() => {
+        location.pathname !== '/rocket' ?
+            $('a')[0].classList.value = 'active' :
+            $('a')[1].classList.value = 'active'
+    })
 
     const activateLink = (e) => {
         let el = e.target
         $(el).siblings().removeClass('active')
         $(el).addClass('active')
-        // console.log($(el.target).siblings());
     }
 
     return <nav onClick={(e) => activateLink(e)}>
-        <Link to="/" className="active">Day/Night</Link>
+        <Link to="/">Day/Night</Link>
         <Link to="/rocket" >Rocket</Link>
     </nav>
 }
